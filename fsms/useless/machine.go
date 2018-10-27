fcb52b8f8fd49e57740b906246e4db72d4233744
yb <yan.babitski@gmail.com>
2018-10-27 00:49:34 -0700

    Try to implement most useless machine using fsm API

--- fsms/useless/machine.go
+++ fsms/useless/machine.go
@@ -2,6 +2,7 @@ package useless
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/looplab/fsm"
 )
@@ -21,13 +22,24 @@ func Create() *MostUselessMachine {
 			{Name: "turn-off", Src: []string{"on"}, Dst: "off"},
 		},
 		fsm.Callbacks{
-			"enter_state": func(e *fsm.Event) { m.enterState(e) },
+			"before_event": m.log,
+			"leave_state":  m.log,
+			"enter_state":  m.log,
+			"after_event":  m.log,
+			"leave_off":    func(_ *fsm.Event) { m.onTurnOn() },
 		},
 	)
 
 	return &m
 }
 
-func (m *MostUselessMachine) enterState(e *fsm.Event) {
-	fmt.Printf("Moving from %s to %s\n", e.Src, e.Dst)
+func (m *MostUselessMachine) log(e *fsm.Event) {
+	fmt.Printf("Event: %v\n", e)
+}
+
+func (m *MostUselessMachine) onTurnOn() {
+	fmt.Println("onTurnOn")
+	if err := m.FSM.Event("turn-off"); err != nil {
+		log.Println(err)
+	}
 }
